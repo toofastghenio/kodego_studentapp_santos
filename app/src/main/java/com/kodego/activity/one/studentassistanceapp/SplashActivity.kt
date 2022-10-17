83a8ddf6a27a7237ad2599a0efecbaa41c22a26c
Eugene santos <evsantos.filigreesales@gmail.com>
2022-10-17 19:11:52 +0800

    add splash screen

---
+++ app/src/main/java/com/kodego/activity/one/studentassistanceapp/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.kodego.activity.one.studentassistanceapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(3000L)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+
+    }
+}
